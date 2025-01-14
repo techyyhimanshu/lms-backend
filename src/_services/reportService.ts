5d9885a2a3736fcc003a16c1564cb77ee99c7507
techyyhimanshu <158696334+techyyhimanshu@users.noreply.github.com>
2025-01-14 11:24:01 +0530

    `Added new report services and routes, deleted multiple excel files`

--- src/_services/reportService.ts
+++ src/_services/reportService.ts
@@ -55,3 +55,54 @@ export const getReportDataLoginWiseService = async (data: any): Promise<any> => 
         throw err
     }
 };
+
+export const getReportCompanyWiseService = async (data: any): Promise<any> => {
+    try {
+        const result = await sequelize.query(
+            `call sp_reportByCompany(:inputCompanyId)`,
+            {
+                replacements: {
+                    inputCompanyId: data.companyId
+                },
+                type: QueryTypes.RAW,
+            }
+        );
+        return result
+    } catch (err) {
+        throw err
+    }
+};
+export const getUserActivityService = async (data: any): Promise<any> => {
+    try {
+        const result = await sequelize.query(
+            `call sp_userActivity(:inputId,:inputDate)`,
+            {
+                replacements: {
+                    inputId: data.id,
+                    inputDate: data.date
+                },
+                type: QueryTypes.RAW,
+            }
+        );
+        return result
+    } catch (err) {
+        throw err
+    }
+};
+
+export const getUserDailyTimeSpendService = async (data: any): Promise<any> => {
+    try {
+        const result = await sequelize.query(
+            `call sp_userDailyTimeSpend(:inputStudentId)`,
+            {
+                replacements: {
+                    inputStudentId: data.id,
+                },
+                type: QueryTypes.RAW,
+            }
+        );
+        return result
+    } catch (err) {
+        throw err
+    }
+};
--- src/controllers/reportController.ts
+++ src/controllers/reportController.ts
@@ -1,7 +1,7 @@
 import { ErrorRequestHandler, RequestHandler } from "express";
 import { AppError } from '../helpers/customError';
 import { sendSuccessResponse } from "../helpers/successResponse";
-import { getReportDataBatchWiseService, getReportDataDateWiseService, getReportDataLoginWiseService } from "../_services/reportService";
+import { getReportCompanyWiseService, getReportDataBatchWiseService, getReportDataDateWiseService, getReportDataLoginWiseService, getUserActivityService, getUserDailyTimeSpendService } from "../_services/reportService";
 
 
 export const getReportDataBatchWise:RequestHandler=async (req,res,next)=>{
@@ -24,6 +24,32 @@ export const getReportDataDateWise:RequestHandler=async (req,res,next)=>{
 export const getReportDataLoginWise:RequestHandler=async (req,res,next)=>{
     try {
         const data=await getReportDataLoginWiseService(req.body)
+        sendSuccessResponse(res,'',data);
+    } catch (error:any) {
+        return next(new AppError(error.message, 400));
+    }
+}
+export const getReportCompanyWise:RequestHandler=async (req,res,next)=>{
+    try {
+        const data=await getReportCompanyWiseService(req.body)
+        sendSuccessResponse(res,'',data);
+    } catch (error:any) {
+        return next(new AppError(error.message, 400));
+    }
+}
+
+export const getUserActivity:RequestHandler=async (req,res,next)=>{
+    try {
+        const data=await getUserActivityService(req.body)
+        sendSuccessResponse(res,'',data);
+    } catch (error:any) {
+        return next(new AppError(error.message, 400));
+    }
+}
+
+export const getUserDailyTimeSpend:RequestHandler=async (req,res,next)=>{
+    try {
+        const data=await getUserDailyTimeSpendService(req.body)
         sendSuccessResponse(res,'',data);
     } catch (error:any) {
         return next(new AppError(error.message, 400));
